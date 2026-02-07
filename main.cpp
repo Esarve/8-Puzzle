72c6acd9bf3c143a772d479a18466fd5711bf498
Sourav Das <sourav@bitsmedia.com>
2026-02-07 14:11:10 -0500

    update varibale names to meaningful names

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #define n 3
 const bool SUCCESS = true;
 using namespace std;
@@ -13,18 +15,18 @@ public:
     f = 0;
     came_from = NULL;
   }
-  static int heuristic (state from, state to) {
-    int ret = 0;
+  static int heuristic (state fromState, state toState) {
+    int mismatchCount = 0;
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
-        if (from.board[i][j] != to.board[i][j])
-          ret++;
-    return ret;
+        if (fromState.board[i][j] != toState.board[i][j])
+          mismatchCount++;
+    return mismatchCount;
   }
-  bool operator == (state a) {
+  bool operator == (state otherState) {
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
-        if (this->board[i][j] != a.board[i][j])
+        if (this->board[i][j] != otherState.board[i][j])
           return false;
     return true;
   }
@@ -38,87 +40,87 @@ public:
   }
 };
 
-vector <state> output;
-bool lowerF (state a, state b) {
-  return a.f < b.f;
+vector <state> solutionPath;
+bool compareFScore (state stateA, state stateB) {
+  return stateA.f < stateB.f;
 }
 
-bool isinset (state a, vector <state> b) {
-  for (int i = 0; i < b.size(); i++)
-    if (a == b[i])
+bool containsState (state targetState, vector <state> stateSet) {
+  for (int i = 0; i < stateSet.size(); i++)
+    if (targetState == stateSet[i])
       return true;
   return false;
 }
 
-void addNeighbor (state current, state goal, int newi, int newj, int posi, int posj, vector <state>& openset, vector <state> closedset) {
-  state newstate = current;
-  swap (newstate.board[newi][newj], newstate.board[posi][posj]);
-  if (!isinset(newstate, closedset) && !isinset(newstate, openset)) {
-      newstate.g = current.g + 1;
-      newstate.f = newstate.g + state :: heuristic(newstate, goal);
-      state* temp = new state();
-      *temp = current;
-      newstate.came_from = temp;
-      openset.push_back(newstate);
+void addNeighborState (state currentState, state goalState, int newRow, int newCol, int emptyRow, int emptyCol, vector <state>& openSet, vector <state> closedSet) {
+  state neighborState = currentState;
+  swap (neighborState.board[newRow][newCol], neighborState.board[emptyRow][emptyCol]);
+  if (!containsState(neighborState, closedSet) && !containsState(neighborState, openSet)) {
+      neighborState.g = currentState.g + 1;
+      neighborState.f = neighborState.g + state :: heuristic(neighborState, goalState);
+      state* parentState = new state();
+      *parentState = currentState;
+      neighborState.came_from = parentState;
+      openSet.push_back(neighborState);
   }
 }
 
-void neighbors (state current, state goal, vector <state>& openset, vector <state>& closedset) {
-  int i, j, posi ,posj;
+void generateNeighbors (state currentState, state goalState, vector <state>& openSet, vector <state>& closedSet) {
+  int i, j, emptyRow ,emptyCol;
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
-      if (current.board[i][j] == 0) {
-        posi = i;
-        posj = j;
+      if (currentState.board[i][j] == 0) {
+        emptyRow = i;
+        emptyCol = j;
         break;
       }
-  i = posi, j = posj;
+  i = emptyRow, j = emptyCol;
   if (i - 1 >= 0)
-    addNeighbor(current, goal, i - 1, j, posi, posj, openset, closedset);
+    addNeighborState(currentState, goalState, i - 1, j, emptyRow, emptyCol, openSet, closedSet);
   if (i + 1 < n)
-    addNeighbor(current, goal, i + 1, j, posi, posj, openset, closedset);
+    addNeighborState(currentState, goalState, i + 1, j, emptyRow, emptyCol, openSet, closedSet);
   if (j + 1 < n)
-    addNeighbor(current, goal, i, j + 1, posi, posj, openset, closedset);
+    addNeighborState(currentState, goalState, i, j + 1, emptyRow, emptyCol, openSet, closedSet);
   if (j - 1 >= 0)
-    addNeighbor(current, goal, i, j - 1, posi, posj, openset, closedset);
+    addNeighborState(currentState, goalState, i, j - 1, emptyRow, emptyCol, openSet, closedSet);
 }
 
-bool reconstruct_path(state current, vector<state> &came_from) {
-    state *temp = &current;
-    while(temp != NULL) {
-        came_from.push_back(*temp);
-        temp = temp->came_from;
+bool reconstruct_path(state currentState, vector<state> &path) {
+    state *statePtr = &currentState;
+    while(statePtr != NULL) {
+        path.push_back(*statePtr);
+        statePtr = statePtr->came_from;
     }
     return SUCCESS;
 }
 
-bool astar (state start, state goal) {
-  vector <state> openset;
-  vector <state> closedset;
-  state current;
-  start.g = 0;
-  start.f = start.g + state :: heuristic(start, goal);
-  openset.push_back(start);
-  while (!openset.empty()) {
-    sort(openset.begin(), openset.end(), lowerF);
-    current = openset[0];
-    if (current == goal)
-      return reconstruct_path(current, output);
-    openset.erase(openset.begin());
-    closedset.push_back(current);
-    neighbors(current, goal, openset, closedset);
+bool astar (state startState, state goalState) {
+  vector <state> openSet;
+  vector <state> closedSet;
+  state currentState;
+  startState.g = 0;
+  startState.f = startState.g + state :: heuristic(startState, goalState);
+  openSet.push_back(startState);
+  while (!openSet.empty()) {
+    sort(openSet.begin(), openSet.end(), compareFScore);
+    currentState = openSet[0];
+    if (currentState == goalState)
+      return reconstruct_path(currentState, solutionPath);
+    openSet.erase(openSet.begin());
+    closedSet.push_back(currentState);
+    generateNeighbors(currentState, goalState, openSet, closedSet);
   }
   return !SUCCESS;
 }
 
 int main () {
-  state start, goal;
+  state startState, goalState;
   freopen("in.txt", "r", stdin);
-  for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> start.board[i][j];
-  for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> goal.board[i][j];
-  if (astar(start, goal) == SUCCESS) {
-    for (int i = output.size() - 1; i >= 0; i--)
-      output[i].print();
+  for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> startState.board[i][j];
+  for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> goalState.board[i][j];
+  if (astar(startState, goalState) == SUCCESS) {
+    for (int i = solutionPath.size() - 1; i >= 0; i--)
+      solutionPath[i].print();
     cout << "SUCKsess" << endl;
   }
   else cout << "FAIL" << endl;
